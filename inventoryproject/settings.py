7c1a3ac70505a3ed5fdc617da5c8e0546e94f070
victorjeol <victorjoelk20@gmail.com>
2023-08-23 20:13:38 -0700

    tweaked app for heroku

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+import django_heroku
+import dj_database_url
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'inventoryproject.urls'
@@ -125,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 STATICFILES_DIRS = (
@@ -160,3 +166,8 @@ EMAIL_HOST_USER = 'precious@laveragellc.com'
 EMAIL_HOST_PASSWORD = 'Precious2022'
 EMAIL_PORT = 465
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+
+
+
+django_heroku.settings(locals())
